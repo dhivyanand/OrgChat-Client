a46d7468a3c9e7197f2f5526a4e2c33c125b7d84
dhivyanand <dhivyananthan98@gmail.com>
2018-11-28 15:54:40 +0530

    Created and initialised Grid View for the Home Activity.

--- app/src/main/java/com/example/system/orgchat_client/home.java
+++ app/src/main/java/com/example/system/orgchat_client/home.java
@@ -2,12 +2,18 @@ package com.example.system.orgchat_client;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class home extends AppCompatActivity {
+
+    GridView grid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        grid = (GridView)findViewById(R.id.home_grid);
+
     }
 }
