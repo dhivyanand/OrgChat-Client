c782e93a2c4e7f1a56246157f3d21a489b7627ea
dhivyanand <dhivyananthan98@gmail.com>
2018-11-28 17:13:12 +0530

    Initialised variables for "getView()" function in home adapter.

--- app/src/main/java/com/example/system/orgchat_client/HomeAdapter.java
+++ app/src/main/java/com/example/system/orgchat_client/HomeAdapter.java
@@ -1,14 +1,24 @@
 package com.example.system.orgchat_client;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by System on 28/11/18.
  */
 
 public class HomeAdapter extends BaseAdapter {
+
+    Context c;
+    public HomeAdapter(Context c){
+        this.c = c;
+    }
+
     @Override
     public int getCount() {
         return 0;
@@ -26,6 +36,13 @@ public class HomeAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
+
+        LayoutInflater layoutInflater = LayoutInflater.from(c);
+        View root = layoutInflater.inflate(R.layout.home_adapter_layout,null);
+
+        ImageView icon = (ImageView)root.findViewById(R.id.icon);
+        TextView title = (TextView)root.findViewById(R.id.title);
+
         return null;
     }
 }
