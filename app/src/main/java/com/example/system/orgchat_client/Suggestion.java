2666540a1eab3ec067d983ca998637cc1a1ead1c
dhivyanand <dhivyananthan98@gmail.com>
2018-11-29 18:36:29 +0530

    Declared instances for each views in Suggestion activity chat room layout.

--- app/src/main/java/com/example/system/orgchat_client/Suggestion.java
+++ app/src/main/java/com/example/system/orgchat_client/Suggestion.java
@@ -2,8 +2,15 @@ package com.example.system.orgchat_client;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class Suggestion extends AppCompatActivity {
+
+    ListView chatView;
+    EditText suggestion;
+    Button send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
