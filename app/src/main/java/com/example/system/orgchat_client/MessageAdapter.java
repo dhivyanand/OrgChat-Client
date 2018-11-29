aba38f56383ad9d03f5771081014e89df4b05c11
dhivyanand <dhivyananthan98@gmail.com>
2018-11-29 18:57:03 +0530

    Developed Constructor for MessageAdapter.

--- app/src/main/java/com/example/system/orgchat_client/MessageAdapter.java
+++ app/src/main/java/com/example/system/orgchat_client/MessageAdapter.java
@@ -1,14 +1,22 @@
 package com.example.system.orgchat_client;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Created by System on 29/11/18.
  */
 
 public class MessageAdapter extends BaseAdapter {
+
+    MessageAdapter(Context c , ArrayList<String> message , ArrayList<Character> direction){
+
+    }
+
     @Override
     public int getCount() {
         return 0;
