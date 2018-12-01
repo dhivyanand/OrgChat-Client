3eb7a7e09dae2ba21c0c6b6e6993d471eeaf3d8c
dhivyanand <dhivyananthan98@gmail.com>
2018-12-01 20:20:40 +0530

    Devleoped Admin activity listview adapter.

---
+++ app/src/main/java/com/example/system/orgchat_client/Admin.java
@@ -0,0 +1,13 @@
+package com.example.system.orgchat_client;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Admin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+    }
+}
