f9d4f976c30770150da1dea257b38b0f90500d52
dhivyanand <dhivyananthan98@gmail.com>
2018-12-01 20:33:12 +0530

    Completed Adapter for Admin ListView.

--- app/src/main/java/AdminListAdapter.java
+++ app/src/main/java/AdminListAdapter.java
@@ -1,8 +1,15 @@
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.system.orgchat_client.R;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -15,6 +22,10 @@ public class AdminListAdapter extends BaseAdapter {
     Context c;
     ArrayList<String> name , notification;
     ArrayList<Bitmap> image;
+
+    ImageView icon;
+    TextView text_name,text_notification;
+
 
     AdminListAdapter(Context c , ArrayList<String> name , ArrayList<String> notification , ArrayList<Bitmap> image){
         this.c = c;
@@ -39,6 +50,17 @@ public class AdminListAdapter extends BaseAdapter {
     }
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
+        LayoutInflater layoutInflater = (LayoutInflater)c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View root = layoutInflater.inflate(R.layout.adminlist_adapter,null);
+
+        icon = (ImageView)root.findViewById(R.id.image);
+        text_name = (TextView)root.findViewById(R.id.name);
+        text_notification = (TextView)root.findViewById(R.id.notification);
+
+        text_name.setText(name.get(i).toString());
+        text_notification.setText(notification.get(i).toString());
+        icon.setImageBitmap(image.get(i));
+
         return null;
     }
 }
