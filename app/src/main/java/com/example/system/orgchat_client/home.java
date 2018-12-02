7554d8d5848222c4060a7dc67ba682911eb0fe0c
dhivyanand <dhivyananthan98@gmail.com>
2018-12-02 13:05:24 +0530

    Added with a conditional statement statement for grid view onclick listener.

--- app/src/main/java/com/example/system/orgchat_client/home.java
+++ app/src/main/java/com/example/system/orgchat_client/home.java
@@ -25,7 +25,9 @@ public class home extends AppCompatActivity {
                 if (i == 1)
                     startActivity(new Intent(home.this,Announcements.class));
                 if (i == 2)
-                    startActivity(new Intent(home.this,Admin.class));
+                    startActivity(new Intent(home.this,Admin.class).putExtra("type","suggestion"));
+                if (i == 3)
+                    startActivity(new Intent(home.this,Admin.class).putExtra("type","compliant"));
             }
         });
 
