5a123ca38f85304e6e181fa6ec3ce2a640138a46
dhivyanand <dhivyananthan98@gmail.com>
2018-12-03 10:33:18 +0530

    Added with status textview on announcement list view.

--- app/src/main/java/com/example/system/orgchat_client/Announcements.java
+++ app/src/main/java/com/example/system/orgchat_client/Announcements.java
@@ -3,11 +3,13 @@ package com.example.system.orgchat_client;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -28,14 +30,14 @@ public class Announcements extends AppCompatActivity {
         ArrayList<Bitmap> i = new ArrayList<Bitmap>();
         Bitmap icon = BitmapFactory.decodeResource(Announcements.this.getResources(),
                 R.mipmap.menu_announcement_foreground);
-        i.add(icon);
-        i.add(icon);
-        list.setAdapter(new AnnouncementListAdapter(Announcements.this,t,i));
+        i.add(((BitmapDrawable)getResources().getDrawable(R.mipmap.menu_announcement_foreground)).getBitmap());
+        i.add(((BitmapDrawable)getResources().getDrawable(R.mipmap.menu_announcement_foreground)).getBitmap());
+        AnnouncementListAdapter announcement = new AnnouncementListAdapter(Announcements.this,t,i);
+        list.setAdapter(announcement);
 
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
                 Intent notification = new Intent(Announcements.this,Notifications.class);
                 notification.putExtra("id","1");
                 startActivity(notification);
