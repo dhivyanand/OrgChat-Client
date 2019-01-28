3a730a6646db6976c5088d26750cb30fb4452b84
dhivyanand <dhivyananthan98@gmail.com>
2019-01-28 14:52:46 +0530

    Created fragment "SubDeptFragment".

---
+++ app/src/main/java/com/example/system/orgchat_client/SubDeptFragment.java
@@ -0,0 +1,31 @@
+package com.example.system.orgchat_client;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SubDeptFragment extends Fragment {
+
+    public SubDeptFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sub_dept, container, false);
+    }
+
+}
