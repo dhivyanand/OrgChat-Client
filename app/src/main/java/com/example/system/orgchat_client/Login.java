43bba5755c4a15f3596d5c58589ae24a255fb421
dhivyanand <dhivyananthan98@gmail.com>
2019-01-28 14:04:42 +0530

    Created "HomeNav" activity.

--- app/src/main/java/com/example/system/orgchat_client/Login.java
+++ app/src/main/java/com/example/system/orgchat_client/Login.java
@@ -5,22 +5,6 @@ import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class Login extends AppCompatActivity {
 
--- app/src/main/java/com/example/system/orgchat_client/home.java
+++ app/src/main/java/com/example/system/orgchat_client/home.java
@@ -18,5 +18,6 @@ public class home extends AppCompatActivity {
 
 
 
+
     }
 }
