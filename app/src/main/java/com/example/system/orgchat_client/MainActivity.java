d98e43db8a67d67db93a4bb678fbd568f13f9f83
dhivyanand <dhivyananthan98@gmail.com>
2019-01-28 14:38:49 +0530

    Added icons for navigation drawer.

--- app/src/main/java/com/example/system/orgchat_client/MainActivity.java
+++ app/src/main/java/com/example/system/orgchat_client/MainActivity.java
@@ -20,45 +20,11 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Context c;
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        c = MainActivity.this;
-
-        int i=0;
-        String[] perms = {"android.permission.WRITE_EXTERNAL_STORAGE","android.permission.CAMERA"};
-
-        while(i<2) {
-            if (ContextCompat.checkSelfPermission(MainActivity.this, perms[i])
-                    != PackageManager.PERMISSION_GRANTED) {
-
-                int permsRequestCode = 200;
-                requestPermissions(perms, permsRequestCode);
-
-            } else {
-
-                finish();
-            }
-            i++;
-        }
-
-    }
-    @Override
-    public void onRequestPermissionsResult(int permsRequestCode, String[] permissions, int[] grantResults){
-
-        boolean a = grantResults[0]==PackageManager.PERMISSION_GRANTED;
-        boolean b = grantResults[1]==PackageManager.PERMISSION_GRANTED;
-
-        if( a==false || b == false)
-            ((AppCompatActivity)c).finish();
-        else
-            startActivity(new Intent(c, MainActivity.class));
-            finish();
+        startActivity(new Intent(MainActivity.this,home.class));
 
     }
 
