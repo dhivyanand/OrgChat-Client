1e4ddedc37088a13442b9318a3b3aa8438e80323
dhivyanand <dhivyananthan98@gmail.com>
2019-02-20 09:49:47 +0530

    Created adapter "SwipeAdapter".

---
+++ app/src/main/java/com/example/system/orgchat_client/Adapters/SwipeAdapter.java
@@ -0,0 +1,34 @@
+package com.example.system.orgchat_client.Adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.example.system.orgchat_client.Fragments.CompliantSwipeFragment;
+import com.example.system.orgchat_client.Fragments.SuggestionSwipeFragment;
+
+public class SwipeAdapter extends FragmentPagerAdapter {
+
+    public SwipeAdapter(FragmentManager fm){
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch(position){
+
+            case 0:
+                return new SuggestionSwipeFragment();
+            case 1:
+                return new CompliantSwipeFragment();
+
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+}
