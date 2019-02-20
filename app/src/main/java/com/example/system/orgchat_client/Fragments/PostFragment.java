f74c128ef070c32fd40362370ed24fc87786e68c
dhivyanand <dhivyananthan98@gmail.com>
2019-02-20 09:50:41 +0530

    Declared variables in Post Fragment.

--- app/src/main/java/com/example/system/orgchat_client/Fragments/PostFragment.java
+++ app/src/main/java/com/example/system/orgchat_client/Fragments/PostFragment.java
@@ -4,14 +4,23 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.system.orgchat_client.Activities.HomeNav;
+import com.example.system.orgchat_client.Adapters.SwipeAdapter;
 import com.example.system.orgchat_client.R;
 
 public class PostFragment extends Fragment {
+
+    ActionBar actionBar;
+    SwipeAdapter swipeAdapter;
+    ViewPager pager;
+
+    String tabs[] = {"Suggestion" , "Compliant"};
 
     public PostFragment() {
         // Required empty public constructor
