6db68622a3ef7682b82c9e47cad81dbd0c1f57b9
dhivyanand <dhivyananthan98@gmail.com>
2019-03-20 11:29:21 +0530

    Created activity "NewCompliant" and its resource files.

---
+++ app/src/main/java/com/example/system/orgchat_client/Activities/NewCompliant.java
@@ -0,0 +1,15 @@
+package com.example.system.orgchat_client.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.system.orgchat_client.R;
+
+public class NewCompliant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_compliant);
+    }
+}
